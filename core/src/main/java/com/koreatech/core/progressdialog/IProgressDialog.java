e0cd9183caba29a67e4439be749b22d795d90d06
Yunjae-Na <yunjaena@gmail.com>
2020-04-22 16:44:46 +0900

    Add custom progress dialog
    Add custom progress dialog

---
+++ core/src/main/java/com/koreatech/core/progressdialog/IProgressDialog.java
@@ -0,0 +1,12 @@
+package com.koreatech.core.progressdialog;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+
+public interface IProgressDialog {
+    void showProgressDialog(@Nullable String message);
+
+    void showProgressDialog(@StringRes int resId);
+
+    void hideProgressDialog();
+}
