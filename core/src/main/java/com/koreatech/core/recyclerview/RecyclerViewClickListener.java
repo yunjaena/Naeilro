720b539fcdbc7e7714de181bb87951ccbb309530
Yunjae-Na <yunjaena@gmail.com>
2020-04-22 17:29:07 +0900

    Add base recyclerview
    Add base recyclerview

---
+++ core/src/main/java/com/koreatech/core/recyclerview/RecyclerViewClickListener.java
@@ -0,0 +1,9 @@
+package com.koreatech.core.recyclerview;
+
+import android.view.View;
+
+public interface RecyclerViewClickListener {
+    void onClick(View view, int position); //click event
+
+    void onLongClick(View view, int position); //long click event
+}
