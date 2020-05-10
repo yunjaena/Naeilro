88c525d62cd3153b806d78ef8d5ac38fcf531998
Yunjae-Na <yunjaena@gmail.com>
2020-05-11 04:02:06 +0900

    Add getTrainArrivalDepartInfo comment
    Add getTrainArrivalDepartInfo comment

--- app/src/main/java/com/koreatech/naeilro/network/service/TrainService.java
+++ app/src/main/java/com/koreatech/naeilro/network/service/TrainService.java
@@ -10,9 +10,23 @@ import retrofit2.http.Query;
 public interface TrainService {
     String TRAIN_ARRIVAL_DEPART_INFO_PATH = "getStrtpntAlocFndTrainInfo";
 
+    /**
+     *
+     * @param serviceKey 서비스 키
+     * @param numOfRows 한 페이지 결과 수
+     * @param pageNo 페이지 번호
+     * @param depPlaceId 출발지ID
+     * @param arrPlaceId 도착지ID
+     * @param depPlandTime 출발일
+     * @param trainGradeCode 차량종류코드
+     * @return
+     */
     @GET(TRAIN_ARRIVAL_DEPART_INFO_PATH)
     @Xml
     Observable<TrainArrivalDepartInfo> getTrainArrivalDepartInfo(@Query("serviceKey")String serviceKey, @Query("numOfRows")int numOfRows,
-                                                                 @Query("pageNo")int pageNo, @Query("depPlaceId") String depPlaceId, @Query("arrPlaceId")String arrPlaceId, @Query("depPlandTime")String depPlandTime, @Query("trainGradeCode") String trainGradeCode);
+                                                                 @Query("pageNo")int pageNo, @Query("depPlaceId") String depPlaceId,
+                                                                 @Query("arrPlaceId")String arrPlaceId, @Query("depPlandTime")String depPlandTime,
+                                                                 @Query("trainGradeCode") String trainGradeCode);
+
 
 }
