6a5549d1245e62e7d207747a0e7a3d3a462dfa7e
주동민 <dongmin960903@gmail.com>
2020-05-12 02:53:37 +0900

    Add category fragment screen change function

--- app/src/main/java/com/koreatech/naeilro/MainActivity.java
+++ app/src/main/java/com/koreatech/naeilro/MainActivity.java
@@ -1,34 +1,27 @@
 package com.koreatech.naeilro;
 
 import android.os.Bundle;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.nailro_toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setElevation(0);
 
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-               R.id.navigation_home, R.id.navigation_shared_info, R.id.navigation_my_plan, R.id.navigation_my_info)
-              .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-
     }
 
 }
