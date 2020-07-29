ce602643ba71fbaa15b77f99441785b91c4079cc
주동민 <dongmin960903@gmail.com>
2020-07-30 01:11:26 +0900

    add enroll service

--- app/src/main/java/com/koreatech/naeilro/network/service/EnrollService.java
+++ app/src/main/java/com/koreatech/naeilro/network/service/EnrollService.java
@@ -1,15 +1,21 @@
 package com.koreatech.naeilro.network.service;
 
+import com.google.common.net.MediaType;
 import com.google.gson.JsonObject;
+import com.koreatech.core.network.Json;
 import com.koreatech.naeilro.network.entity.enroll.EnrollObject;
 
 import io.reactivex.Observable;
-import retrofit2.Call;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface EnrollService {
-    String ENROLL_USER = "/register.php";
+    String ENROLL_USER = "/nailo/member/register.php";
+
     @POST(ENROLL_USER)
-    Observable<EnrollObject> postEnroll(@Body EnrollObject jsonObject);
+    @Json
+    Observable<EnrollObject> postEnroll(@Body RequestBody requestBody);
+
 }
